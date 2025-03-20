6ed8aecae83440585f21e89c80f54d83fb569b95
Khalood301 <khaloodbhatti337@gmail.com>
2025-03-20 17:23:43 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello from both branches merged!");
 
->>>>>>> feature-branch
+
 
     }
 }
